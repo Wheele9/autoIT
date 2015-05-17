22093ccadd7c42a8593574787dc9ede8325a9386
Czeman Matyas <cz.matyas@gmail.com>
2015-05-17 13:03:20 +0200

    autoIT script runs noproblem :)

--- autoIT/autoIT.py
+++ autoIT/autoIT.py
@@ -1,9 +1,10 @@
 ##Send("{LWINDOWN}{l}{LWINUP}")
 import subprocess
 import os
-os.startfile(r"C:\Program Files\Internet Explorer\iexplore.exe")  # THIS ONE WORKS !!!
+#os.startfile(r"C:\Program Files\Internet Explorer\iexplore.exe")  # THIS ONE WORKS !!!
 
 print 66
-
+os.startfile(r"C:\Users\Matyi\OneDrive\autoIT\muteWINDOWS.exe") # THIS ONE TOO !!!
+print 55
 
 ##subprocess.Popen(r"C:\Program Files\Internet Explorer\iexplore.exe")
