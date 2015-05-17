d8633659dd94c24cb0af002bd512fbcb849e4a03
Czeman Matyas <cz.matyas@gmail.com>
2015-05-17 11:29:20 +0200

    Frist commit, running executable (.exe) works great.

---
+++ autoIT/autoIT.py
@@ -0,0 +1,9 @@
+##Send("{LWINDOWN}{l}{LWINUP}")
+import subprocess
+import os
+os.startfile(r"C:\Program Files\Internet Explorer\iexplore.exe")  # THIS ONE WORKS !!!
+
+print 66
+
+
+##subprocess.Popen(r"C:\Program Files\Internet Explorer\iexplore.exe")
